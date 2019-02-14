9c51559f652448fc84cc07b5cfe1a6c4466f16ca
Prateek Dasgupta <f20160602@hyderabad.bits-pilani.ac.in>
2019-02-14 20:19:17 +0530

    Data Preprocessing

--- FP.py
+++ FP.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib as plt
 import csv
 import operator
+from collections import OrderedDict
 
 with open('groceries.csv') as csv_file:
     csv_reader = csv.reader(csv_file)
@@ -19,19 +20,55 @@ unique_sets=counts.to_dict()
 sorted_items = sorted(unique_sets.items(), key=operator.itemgetter(1), reverse=True)
  
 itemsets=[]
-    
+row=rows[3]   
 for row in rows:
     l=[]
     for value in row:
         l.append(unique_sets[value])
     d = dict(zip(row, l))
     sorted_items = sorted(d.items(), key=operator.itemgetter(1), reverse=True)
-    itemsets.append(sorted_items)
+    itemsets.append(list((x[0] for x in sorted_items)))
     
-    
+ItemFrame=pd.DataFrame(itemsets)
 
-"""I have unique items in the form of a dictionary
-"""
+   
+    
+    
+ """   
+    
+class Node:
+    def __init__(self, dataval=None):
+        self.dataval = dataval
+        self.nextval = None
+        
+class SLinkedList:
+    def __init__(self):
+        self.headval = None
+
+    def listprint(self):
+        printval = self.headval
+        while printval is not None:
+            print (printval.dataval)
+            printval = printval.nextval
+
+list = SLinkedList()
+list.headval = Node([0])
+e2 = Node("Tue")
+e3 = Node("Wed")
+
+row=itemsets[0]
+len(row["ready soups"])
+# Link first Node to second node
+list.headval.nextval = e2
+
+# Link second Node to third node
+e2.nextval = e3
+
+list.listprint()
+
+
+
+
 
 class Tree:
     def __init__(self):
@@ -47,3 +84,4 @@ root.left = Tree()
 root.left.data = "left"
 root.right = Tree()
 root.right.data = "right"
+"""
